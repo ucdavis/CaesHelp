ac5c00e7af9930f1a6db5861f126e35fc60b817a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-01-25 13:22:12 -0800

    Working examples of recaptcha (v2)

---
+++ src/CaesHelp/Controllers/TestController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CaesHelp.Models;
+using Microsoft.AspNetCore.Mvc;
+using PaulMiami.AspNetCore.Mvc.Recaptcha;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CaesHelp.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ValidateRecaptcha]
+        [HttpPost]
+        public IActionResult Index(TestEmailNewsletterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("ThankYou", new { name = viewModel.Name });
+            }
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult JqueryValidationDisabled()
+        {
+            return View();
+        }
+
+        [ValidateRecaptcha]
+        [HttpPost]
+        public IActionResult JqueryValidationDisabled(TestEmailNewsletterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(ThankYou), new { name = viewModel.Name });
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Compact()
+        {
+            return View();
+        }
+
+        [ValidateRecaptcha]
+        [HttpPost]
+        public IActionResult Compact(TestEmailNewsletterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(ThankYou), new { name = viewModel.Name });
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Audio()
+        {
+            return View();
+        }
+
+        [ValidateRecaptcha]
+        [HttpPost]
+        public IActionResult Audio(TestEmailNewsletterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(ThankYou), new { name = viewModel.Name });
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ThankYou(string name)
+        {
+            return View(nameof(ThankYou), name);
+        }
+    }
+}
---
+++ src/CaesHelp/Models/TestModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaesHelp.Models
+{
+    public class TestEmailNewsletterViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
