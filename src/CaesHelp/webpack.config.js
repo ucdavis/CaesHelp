682400d83e308532aac37f4d16e9d30e3544bd6a
Unknown <srkirkland@ucdavis.edu>
2019-06-25 10:57:18 -0700

    new webpack and deps

---
+++ src/CaesHelp/webpack.config.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CheckerPlugin = require("awesome-typescript-loader").CheckerPlugin;
+const bundleOutputDir = "./wwwroot/dist";
+
+module.exports = env => {
+  const isDevBuild = !(env && env.prod);
+  return [
+    {
+      stats: { modules: false },
+      entry: {
+        main: "./ClientApp/boot.tsx",
+        app: "./ClientApp/app.tsx",
+        vendor: [
+          "event-source-polyfill",
+          "isomorphic-fetch",
+          "react",
+          "react-dom"
+        ]
+      },
+      resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+      output: {
+        path: path.join(__dirname, bundleOutputDir),
+        filename: "[name].js",
+        publicPath: "/"
+      },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            include: /ClientApp/,
+            use: "awesome-typescript-loader?silent=true"
+          },
+          {
+            test: /\.css$/,
+            use: isDevBuild
+              ? ["style-loader", "css-loader"]
+              : ExtractTextPlugin.extract({ use: "css-loader?minimize" })
+          },
+          {
+            test: /\.scss$/,
+            use: isDevBuild
+              ? ["style-loader", "css-loader", "sass-loader"]
+              : ExtractTextPlugin.extract({
+                  use: ["css-loader", "sass-loader"]
+                })
+          },
+          { test: /\.(png|jpg|jpeg|gif|svg|woff)$/, use: "url-loader?limit=25000" }
+        ]
+      },
+      plugins: [
+        new CheckerPlugin(),
+        new webpack.DefinePlugin({
+          "process.env.NODE_ENV": isDevBuild ? '"development"' : '"production"'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+          name: "vendor",
+          minChunks: Infinity
+        })
+      ].concat(
+        isDevBuild
+          ? [
+              // Plugins that apply in development builds only
+              new webpack.SourceMapDevToolPlugin({
+                filename: "[file].map", // Remove this line if you prefer inline source maps
+                moduleFilenameTemplate: path.relative(
+                  bundleOutputDir,
+                  "[resourcePath]"
+                ) // Point sourcemap entries to the original file locations on disk
+              })
+            ]
+          : [
+              // Plugins that apply in production builds only
+              new webpack.optimize.UglifyJsPlugin(),
+              new ExtractTextPlugin("site.css")
+            ]
+      )
+    }
+  ];
+};
