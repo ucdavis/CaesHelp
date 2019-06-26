15b5532bad3329c362ec435e1d2f5800037dcc05
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-06-26 08:55:37 -0700

    WIP

--- src/CaesHelp/ClientApp/app.tsx
+++ src/CaesHelp/ClientApp/app.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import Ticket from "./components/Ticket";
 
 function renderApp() {
   // This code starts up the React app when it runs in a browser. It sets up the routing
   // configuration and injects the app into a DOM element.
   ReactDOM.render(
     <AppContainer>
-        <h1>Hello world!!</h1>
+        <Ticket/>
     </AppContainer>,
     document.getElementById('react-app')
   );
---
+++ src/CaesHelp/ClientApp/components/Ticket.tsx
@@ -0,0 +1,30 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+
+export default class Ticket extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    public  render() {
+        return(
+            <div>
+                <div className="form-group">
+                    <label className="control-label">Urgency</label>
+                    <select name="UrgencyLevel" className="form-control">
+                        <option value="Non-Critical Issue">Non-Critical Issue</option>
+                        <option value="Scheduled Requests">Scheduled Requests</option>
+                        <option value="Workaround Available">Workaround Available</option>
+                        <option value="Work Stoppage">Work Stoppage</option>
+                        <option value="Critical">Critical</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label className="control-label">Subject</label>
+                    <input type="text" name="Subject" className="form-control"/>
+                </div>    
+            </div>
+        );
+    }
+}
