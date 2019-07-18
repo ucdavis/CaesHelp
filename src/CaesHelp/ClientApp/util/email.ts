a93ce56b8fe2a637adad2f378a5f9bfda4f323b8
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-07-18 13:59:55 -0700

    Email validation

---
+++ src/CaesHelp/ClientApp/util/email.ts
@@ -0,0 +1,6 @@
+﻿export function validateEmail(email: string): boolean {
+    /* tslint:disable-next-line:max-line-length */
+    // per: https://www.regular-expressions.info/email.html
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/;
+    return regex.test(email);
+}
