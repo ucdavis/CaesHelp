12290ce7d9482533993743b55724839f38fbdbca
Jason Sylvestre <jsylvestre@ucdavis.edu>
2020-02-06 13:14:42 -0800

    More pretty

--- src/CaesHelp/ClientApp/components/ErrorList.tsx
+++ src/CaesHelp/ClientApp/components/ErrorList.tsx
@@ -1,13 +1,15 @@
-﻿import * as React from "react";
+﻿import * as React from 'react';
 
 interface IProps {
-    errorArray: [string];
+  errorArray: [string];
 }
 
 export default function ErrorList(props: IProps) {
-    const errors = props.errorArray;
-    const errorList = errors.map((error) =>
-        <li key={error}>{error}</li>
-    );
-    return (<div className="alert alert-danger"><ul>{errorList}</ul></div>);
+  const errors = props.errorArray;
+  const errorList = errors.map(error => <li key={error}>{error}</li>);
+  return (
+    <div className="alert alert-danger">
+      <ul>{errorList}</ul>
+    </div>
+  );
 }
