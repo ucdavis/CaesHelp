1953d52ad71fa3825d65d0c69e261c9fb087f89c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2026-01-21 10:26:49 -0800

    Refactors application list to use data file
    Moves the list of applications and their display names to a dedicated data file.
    
    This improves maintainability and makes it easier to update the list of applications in the ticket form.

---
+++ src/CaesHelp/ClientApp/src/data/applications.ts
@@ -0,0 +1,39 @@
+export const applications = [
+  'Academic Course Evaluations',
+  'AD419',
+  'Anlab',
+  'Registration',
+  'Eat Fit',
+  'Eligibility List',
+  'Employee Salary Review Analysis',
+  'Fleece',
+  'Payments',
+  'Peaks',
+  'PrePurchasing',
+  'PTF',
+  'Student Information Management System',
+  'Subject To Dismissal',
+  'TPS3',
+  'Tacos',
+  'WHO',
+  'Download',
+  'Data Dictionary',
+  'Harvest',
+  'Hippo',
+  'Finjector',
+  'PolicyWonk',
+  'Sloth',
+  'Walter'
+];
+
+// Application display names (if different from value)
+export const applicationDisplayNames: { [key: string]: string } = {
+  Anlab: 'Anlab (TOPS)',
+  Fleece: 'Fleece (Faculty Directory)',
+  WHO: 'Who.ucdavis.edu',
+  Download: 'download.ucdavis.edu',
+  Hippo: 'Hippo - High Performance Personnel Onboarding',
+  Finjector: 'Finjector - Aggie Enterprise Financial Account Picker',
+  PolicyWonk: 'Policy Wonk - Your UC Policy Expert',
+  Walter: 'Walter - All your reporting needs'
+};
